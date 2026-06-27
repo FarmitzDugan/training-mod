123280d286e8a14dd47aaef637b89f778218239d
FarmitzDugan <53383471+FarmitzDugan@users.noreply.github.com>
2026-06-27 16:47:15 -0700

    Create initial Fabric mod projecct

---
+++ src/client/java/io/github/farmitzdugan/trainingmod/client/TrainingModClient.java
@@ -0,0 +1,10 @@
+package io.github.farmitzdugan.trainingmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TrainingModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
